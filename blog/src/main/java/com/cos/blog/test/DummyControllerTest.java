0c47e1a528dad540a38694e266eed336411f87f4
sjhh0712 <sjhh0712@naver.com>
2022-11-07 17:49:47 +0900

    delete(@DeleteMapping)으로 삭제하기

--- blog/src/main/java/com/cos/blog/test/DummyControllerTest.java
+++ blog/src/main/java/com/cos/blog/test/DummyControllerTest.java
@@ -6,10 +6,12 @@ import java.util.function.Supplier;
 import javax.transaction.Transactional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -27,15 +29,28 @@ public class DummyControllerTest {
 	@Autowired // 의존성 주입  ->  패키지 스캔 시 현재 컨트롤러가 메모리에 올라가는데 UserRepository는 null상태. 이걸 Autowired가 bean을 주입하여 프로퍼티 사용가능
 	private UserRepository userRepository;
 	
-	@Transactional // 더티 체킹. @Transactional을 사용하면 save함수를 쓰지않아도 update가 됨
+	// 삭제
+	@DeleteMapping("/dummy/user/{id}")
+	public String delete(@PathVariable int id) {
+		try {
+			userRepository.deleteById(id);			
+		}catch(Exception e) {
+			return "삭제에 실패하였습니다. 해당 id는 데이터베이스에 존재하지 않습니다.";
+		}
+		
+		return "삭제되었습니다.";
+	}
+	
+	// update(수정)
+	@Transactional // 더티 체킹. @Transactional을 사용하면 save함수를 쓰지않아도 update가 됨./ 함수 종료시 자동 commit이 됨.
 	@PutMapping("/dummy/user/{id}")
-	public User updateUser(@PathVariable int id, @RequestBody User requestUser) { // @RequestBody 는 Json형시을 받을수 있음
+	public User updateUser(@PathVariable int id, @RequestBody User requestUser) { // @RequestBody 는 Json형식을 받을수 있음
 		// Json 데이터를 요청 => Spring이 Java Object로 받아줌. 이때 필요한 어노테이션이 RequestBody임
 		System.out.println("id : "+ id);
 		System.out.println("password : " + requestUser.getPassword());
 		System.out.println("email : " + requestUser.getEmail());
 		
-		// java는 파라미터에 함수를 넣을수 없지만 람다식을 사용하면 가능
+		// java는 파라미터에 함수를 넣을수 없지만 람다식을 사용하면 가능. 수정하고자 하는 id를 select
 		User user = userRepository.findById(id).orElseThrow(()->{
 			return new IllegalArgumentException("수정에 실패하였습니다.");
 		});
@@ -45,7 +60,7 @@ public class DummyControllerTest {
 		//userRepository.save(user); // save는 Insert할때 쓰는건데 db에 값이 있다면 Update를 해줘버림. 그리고 나머지 데이터를 안넣어주면 null을 넣어줌
 								   // -> save함수는 id를 전달하지 않으면 insert를 해주고, id를 전달하면 해당 id에 대한 데이터가 있을때 update
 								   // 없을때 insert해줌
-		return null;
+		return user;
 	}
 	
 	// user 전체를 반환
@@ -56,7 +71,7 @@ public class DummyControllerTest {
 	
 	// 한페이지당 2건의 데이터를 리턴
 	@GetMapping("/dummy/user")
-	// List를 져와서 User객체 타입으로 반환하겠다.
+	// List를 가져와서 User객체 타입으로 반환하겠다.
 	public List<User> pageList(@PageableDefault(size=2, sort="id", direction=Sort.Direction.DESC) Pageable pageable){
 		// Page를 가져와서 User객체 타입으로 반환하겠다
 		Page<User> pagingUser = userRepository.findAll(pageable);// -> 페이지 정보를 pagingUser에 저장.
